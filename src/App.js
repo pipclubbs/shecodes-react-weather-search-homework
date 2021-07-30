d313714a7b37b26ee9bcfcd41907e6f20ee5c23b
pipclubbs <70511582+pipclubbs@users.noreply.github.com>
2021-07-30 17:49:11 +0100

    creating html page with minimal styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <form>
+            <div className="row">
+              <div className="col-10 w-50">
+                <input
+                  className="form-control search-input"
+                  input="text"
+                  placeholder="Enter a city..."
+                ></input>
+              </div>
+              <div className="col-2 p-0 ">
+                <input
+                  className="btn btn-warning search-button"
+                  input="submit"
+                  placeholder="Search"
+                ></input>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <div className="row">
+              <h1>Paris</h1>
+            </div>
+            <div className="row">
+              <ul className="local-info">
+                <li>Thursday, 17:50, scattered clouds</li>
+                <li>Humidity: 60%, Wind: 15km/h</li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-6">
+            <img src="./sunny.png" alt="weather icon" />
+            <span>21°C</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
