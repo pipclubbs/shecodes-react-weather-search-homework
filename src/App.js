c32278b3497764123da35868e1d1e24aff4f4feb
pipclubbs <70511582+pipclubbs@users.noreply.github.com>
2021-08-03 14:06:26 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Newcastle upon Tyne" />
+        <Weather defaultCity="Paris" />
       </div>
 
       <div>
