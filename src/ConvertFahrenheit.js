974f21f6459348890e931ed9d8a653950c7f0722
pipclubbs <70511582+pipclubbs@users.noreply.github.com>
2021-08-07 18:54:20 +0100

    removed farenheit conversion

--- src/ConvertFahrenheit.js
+++ src/ConvertFahrenheit.js
@@ -1,46 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function ConvertFahrenheit(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function convertToFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function convertToCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="ConvertFahrenheit">
-        <span className="current">{Math.round(props.celsius)}</span>
-        <span className="celsius">
-          <span>
-            °C {""} | {""}
-          </span>
-          <a href="/" onClick={convertToFahrenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    let fahrenheit = (props.celsius * 9) / 5 + 32;
-    return (
-      <div className="ConvertFahrenheit">
-        <span className="current">{Math.round(fahrenheit)}</span>
-        <span className="celsius">
-          <span>
-            °F {""} | {""}
-          </span>
-          <a href="/" onClick={convertToCelsius}>
-            °C
-          </a>
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="ConvertFahrenheit">
+      <span className="current">{Math.round(props.celsius)}</span>
+      <span className="celsius">°C</span>
+    </div>
+  );
 }
